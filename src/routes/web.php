339de41eb257e5c3eafeb3f64d14d7949a502e5e
KenichiTnk <ken.1978.tanaka@gmail.com>
2024-11-02 05:10:29 +0900

    controller,model追記

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\RestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/index', [AttendanceController::class, 'index']);
+    Route::post('/attendancein', [AttendanceController::class, 'attendancein']);
+    Route::post('/attendanceout', [AttendanceController::class, 'attendanceout']);
+    Route::get('/date', [AttendanceController::class, 'dateindex']);
+    Route::post('/date', [AttendanceController::class, 'otherday']);
+    Route::post('/restin', [RestController::class, 'restin']);
+    Route::post('/restout', [RestController::class, 'restout']); 
 });
